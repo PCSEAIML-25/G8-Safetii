e3cd91a52c0e67466beea59818e79d59a38e991f
Devanshu Saxena <86067292+devanshu0605@users.noreply.github.com>
2023-06-22 16:26:37 +0530

    Add files via upload

---
+++ Safetii-ai-doctor/app.js
@@ -0,0 +1,48 @@
+const API_KEY = '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'
+const submitButton = document.querySelector('#submit')
+const outPutElement = document.querySelector('#output')
+const inputElement = document.querySelector('input')
+const historyElement = document.querySelector('.history')
+const buttonElement = document.querySelector('button')
+
+function changeInput(value){
+    const inputElement = document.querySelector('input')
+    inputElement.value = value
+}
+
+async function getMessage(){
+    console.log('clicked')
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: inputElement.value}],
+            max_tokens: 1000
+        })
+    }
+    try{
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log(data)
+        outPutElement.textContent = data.choices[0].message.content
+        if(data.choices[0].message.content){
+            const pElement = document.createElement('p')
+            pElement.textContent= inputElement.value
+            pElement.addEventListener('click', () => changeInput(pElement.textContent))
+            historyElement.append(pElement)
+        }
+
+    } catch (error){
+    console.error(error)
+    }
+}
+function clearInput (){
+    inputElement.value=''
+}
+
+submitButton.addEventListener('click', getMessage)
+buttonElement.addEventListener('click', clearInput)
